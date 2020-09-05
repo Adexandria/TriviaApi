c7beb93522a358446d9c645b8dfc306277fab22a
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2020-09-05 15:28:21 +0100

    Added paging class

---
+++ TriviaApi/Entities/Paging.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TriviaApi.Entities
+{
+    public class Paging
+    {
+        private const int maxPageSizes = 100;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 5;
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > _pageSize) ? maxPageSizes : value;
+        }
+        public string Orderby { get; set; } = "Category";
+        public string Fields { get; set; }
+    }
+}
