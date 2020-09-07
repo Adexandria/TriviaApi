733247e1d1ee891d4debfa5e3b8aa43896b21e79
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2020-09-07 09:56:02 +0100

    Changed Datatype

--- TriviaApi/Model/QuestionsDto.cs
+++ TriviaApi/Model/QuestionsDto.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using TriviaApi.Entities;
 
 namespace TriviaApi.Model
 {
@@ -10,7 +11,7 @@ namespace TriviaApi.Model
     {
         [Key]
         public Guid Id { get; set; }
-
+        public string Category { get; set; }
         public string Question { get; set; }
     }
 }
