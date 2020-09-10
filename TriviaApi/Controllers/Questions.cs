6101b80633a589bace8405a039cd02ec60a68cb0
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2020-09-10 11:25:25 +0100

    Added Controller

---
+++ TriviaApi/Controllers/Questions.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TriviaApi.Entities;
+using TriviaApi.Helpers;
+using TriviaApi.Model;
+using TriviaApi.Service;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TriviaApi.Controllers
+{
+    [ApiController]
+    [Route("api/question")]
+    public class Questions : ControllerBase
+    {
+        private readonly IData data;
+        private readonly IMapper mapper;
+        public Questions(IData data, IMapper mapper)
+        {
+            this.data = data ?? throw new NullReferenceException(nameof(data));
+            this.mapper = mapper ?? throw new NullReferenceException(nameof(mapper));
+        }
+        [HttpGet(Name = "Questions")]
+        [HttpHead]
+        public IActionResult GetQuestions(Paging sizes)
+        {
+            var questions = data.GetTrivia(sizes);
+            if (questions == null)
+            {
+                return NotFound();
+            }
+            var pagingMeta = new
+            {
+                pageSize = questions.PageSize,
+                count = questions.TotalCount,
+                totalPage = questions.TotalPage,
+                current = questions.CurrentPage
+            };
+            Response.Headers.Add("X-Pagination", JsonSerializer.Serialize(pagingMeta));
+            var link = CreateQuestionsLink(sizes, questions.HasNext, questions.Hasprevious);
+            var addedlink = mapper.Map<IEnumerable<QuestionsDto>>(questions);
+            var question = addedlink.Select(a =>
+            {
+                var questionsdictionary = a.ShapeData(sizes.Fields) as IDictionary<string, object>;
+                var newlink = CreateQuestionLink((Guid)questionsdictionary["Id"], null);
+                questionsdictionary.Add("links", newlink);
+                return questionsdictionary;
+            });
+            var linkCollection = new
+            {
+                value = question,
+                link
+            };
+            return Ok(linkCollection);
+        }
+        private string CreateQuestionUrl(Paging sizes, ResourceUriType type)
+        {
+            switch (type)
+            {
+                case ResourceUriType.PreviousPage:
+                    return Url.Link("Questions", new
+                    {
+                        fields = sizes.Fields,
+                        pageNumber = sizes.PageNumber - 1,
+                        pageSize = sizes.PageSize
+                    });
+                case ResourceUriType.Nextpage:
+                    return Url.Link("Questions", new
+                    {
+                        fields = sizes.Fields,
+                        pageNumber = sizes.PageNumber + 1,
+                        pageSize = sizes.PageSize
+                    });
+                case ResourceUriType.Current:
+                default:
+                    return Url.Link("Questions", new
+                    {
+                        fields = sizes.Fields,
+                        pageNumber = sizes.PageNumber,
+                        pageSize = sizes.PageSize
+                    });
+
+
+            }
+
+        }
+        private IEnumerable<LinkDto> CreateQuestionLink(Guid id, string field)
+        {
+            var link = new List<LinkDto>();
+            if (string.IsNullOrWhiteSpace(field))
+            {
+                link.Add(new LinkDto(Url.Link("Question", new { id }), "self", "GET"));
+            }
+            else
+            {
+                link.Add(new LinkDto(Url.Link("Question", new { id, field }), "self", "GET"));
+            }
+            return link;
+        }
+
+        private IEnumerable<LinkDto> CreateQuestionsLink(Paging sizes, bool hasNext, bool hasPrevious)
+        {
+            var links = new List<LinkDto>();
+            links.Add(new LinkDto(CreateQuestionUrl(sizes, ResourceUriType.Current), "self", "GET"));
+
+            if (hasNext)
+            {
+                links.Add(new LinkDto(CreateQuestionUrl(sizes, ResourceUriType.Nextpage), "Nextpage", "GET"));
+            }
+            if (hasPrevious)
+            {
+                links.Add(new LinkDto(CreateQuestionUrl(sizes, ResourceUriType.PreviousPage), "Previouspage", "GET"));
+            }
+            return links;
+        }
+    }
+}
+   
+
+
