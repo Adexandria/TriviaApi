b65d7bf6ba645131cde4dd3526e99fc26ad5e35b
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2020-09-10 11:39:07 +0100

    Added link and answer view

---
+++ TriviaApi/Model/AnswerDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TriviaApi.Model
+{
+    public class AnswerDto
+    {
+        public string Answer { get; set; }
+        
+    }
+}
